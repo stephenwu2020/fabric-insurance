6aba5c093e3d73b6954be94bea42bf97d24bb399
stephenwu2020 <stephenwu2020@outlook.com>
2020-06-23 16:10:55 +0800

    auto routing

--- webapp/src/pages/index.vue
+++ webapp/src/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -8,9 +8,8 @@
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
+  name: 'home',
   components: {
     HelloWorld
   }
