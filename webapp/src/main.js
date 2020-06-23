335a4d81de1fa8c9e036d36260ae59a3e33dcd1d
stephenwu2020 <stephenwu2020@outlook.com>
2020-06-23 16:41:30 +0800

    add axios plugin

--- webapp/src/main.js
+++ webapp/src/main.js
@@ -2,7 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './plugins/element.js'
+
+// plugins
+import '@/plugins/element.js'
+import '@/plugins/axios.js'
 
 Vue.config.productionTip = false
 
---
+++ webapp/src/plugins/axios.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+let baseURL = ''
+switch (process.env.NODE_ENV) {
+  case 'production':
+    baseURL = 'http://localhost:8000/api/v1'
+    break
+  default:
+    baseURL = 'http://localhost:8000/api/v1'
+    break
+}
+
+const instance = axios.create({ baseURL })
+
+instance.interceptors.response.use(
+  response => {
+    return response.data
+  },
+  error => {
+    return error
+  })
+
+Vue.prototype.$axios = instance
+
+export default instance
