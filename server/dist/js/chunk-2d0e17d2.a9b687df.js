c81c7f7507fd96bbee53a6c9edce5fe1deccddc4
stephenwu2020 <stephenwu2020@outlook.com>
2020-06-25 12:07:06 +0800

    index page

--- server/dist/js/chunk-2d0e17d2.a9b687df.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e17d2"],{"7b3d":function(n,e,c){"use strict";c.r(e);var r=function(){var n=this,e=n.$createElement,c=n._self._c||e;return c("div",[c("router-view"),c("p",[n._v("© 2020 Fabric Insurance, Inc")])],1)},t=[],u=c("2877"),a={},i=Object(u["a"])(a,r,t,!1,null,null,null);e["default"]=i.exports}}]);
-//# sourceMappingURL=chunk-2d0e17d2.a9b687df.js.map
