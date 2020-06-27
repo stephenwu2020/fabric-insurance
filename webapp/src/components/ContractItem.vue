5e0757967f3232ef06399f958df0a305e030627b
stephenwu2020 <stephenwu2020@outlook.com>
2020-06-27 15:35:26 +0800

    claim

--- webapp/src/components/ContractItem.vue
+++ webapp/src/components/ContractItem.vue
@@ -5,14 +5,85 @@
     <span class="item-line" v-for="count in 7" :key="count"></span>
     <div class="item-sign">
       <div class="item-sign__circle"></div>
-      <span class="item-sign__line"></span>
+      <div class="item-sign-right">
+        <el-button type="primary" size="mini" round @click="claim">Claim</el-button>
+        <span class="item-sign-right__line"></span>
+      </div>
     </div>
+    <el-dialog title="Create a Claim" 
+      :visible.sync="dialogFormVisible" 
+      :append-to-body="true" 
+      width="500px">
+      <el-form :model="form" :label-width="formLabelWidth" v-loading="loading">
+        <el-form-item label="Contract UUID:">
+          <el-input v-model="item.uuid" autocomplete="off" :disabled="true"></el-input>
+        </el-form-item>
+        <el-form-item label="Is Theft:">
+          <el-switch v-model="form.is_theft"></el-switch>
+        </el-form-item>
+        <el-form-item label="Description:">
+          <el-input v-model="form.description" type="textarea"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="confirm">Confirm</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
 <script>
 export default {
   props: ["item"],
+  data() {
+    return {
+      dialogFormVisible: false,
+      formLabelWidth: '120px',
+      loading: false,
+      form: {
+        is_theft: false,
+        description: '',
+      }
+    }
+  },
+  methods: {
+    claim(){
+      this.dialogFormVisible = true
+    },
+    confirm(){
+      if(!this.form.description){
+        this.$message({
+          message: 'Please full fill the form.',
+          type: 'warning'
+        });
+        return
+      }
+      this.loading = true
+      let ajaxdata = {}
+      Object.assign(ajaxdata, this.form)
+      ajaxdata.contract_uuid = this.item.uuid
+      this.$axios.post('/fileClaim', ajaxdata)
+        .then(res => {
+          if(res.msg == 'success'){
+            this.$message({
+              message: "Claim Success!",
+              type: 'success'
+            })
+          }
+          this.hideDialog()
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
+    hideDialog() {
+      this.dialogFormVisible = false
+      this.loading = false
+      this.form.is_theft = false
+      this.form.description = ''
+    }
+  }
 }
 </script>
 
@@ -51,17 +122,24 @@ export default {
     display: flex;
     justify-content: space-between;
     align-items: flex-end;
+    &-right{
+      width: 60%;
+      display: flex;
+      flex-direction: column;
+      align-items: flex-end;
+      &__line{
+        margin-top: 10px;
+        width: 90%;
+        height: 10px;
+        background: #ccc;
+        display: inline-block; 
+      }
+    }
     &__circle{
       width: 60px;
       height: 60px;
       border-radius: 50%;
       background: #ccc;
-    }
-    &__line{
-      width: 50%;
-      height: 10px;
-      background: #ccc;
-      display: inline-block; 
     }
   }
 }
