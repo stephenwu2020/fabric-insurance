de74e0369b7fd7daf316fa9c70dd3810638a5168
stephenwu2020 <stephenwu2020@outlook.com>
2020-06-30 10:34:30 +0800

    header tab

--- webapp/src/components/Header.vue
+++ webapp/src/components/Header.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="header">
     <router-link class="header-logo" to="/">FabINS</router-link>
-    <router-link class="header-link" to="/shop">Shop</router-link>
-    <router-link class="header-link" to="/contract">Contract</router-link>
-    <router-link class="header-link" to="/repair">Repair</router-link>
-    <router-link class="header-link" to="/police">Police</router-link>
-    <router-link class="header-link" to="/insurance">Insurance</router-link>
+    <router-link class="header-link" to="/shop" :class="{'header-active': activeTab=='shop'}">Shop</router-link>
+    <router-link class="header-link" to="/contract" :class="{'header-active': activeTab=='contract'}">Contract</router-link>
+    <router-link class="header-link" to="/repair" :class="{'header-active': activeTab=='repair'}">Repair</router-link>
+    <router-link class="header-link" to="/police" :class="{'header-active': activeTab=='police'}">Police</router-link>
+    <router-link class="header-link" to="/insurance" :class="{'header-active': activeTab=='insurance'}">Insurance</router-link>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeTab: 'contract'
+    }
+  },
+  watch: {
+    '$route': 'onRouteChange'
+  },
+  created(){
+    this.onRouteChange()
+  },
+  methods: {
+    onRouteChange(){
+      let pathArr = this.$route.path.split('/')
+      this.activeTab = pathArr[1]
+    }
+  }
+}
+</script>
 
 <style lang="postcss" scoped>
 .header{
@@ -29,6 +51,18 @@
     font-weight: bolder;
     padding: 0 40px;
     font-size: 24px;
+  }
+  &-active{
+    position: relative;
+    &:after{
+      position: absolute;
+      bottom: -15px;
+      left: 10%;
+      right: 10%;
+      content: '';
+      height: 5px;
+      background: #3a86ff;
+    }
   }
 }
 </style>
